e5230bac68811fa1556e878852e6f71b2cb79ad8
zeus156 <benneyayassine@gmail.com>
2025-07-23 11:28:00 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Villamobilidesign',
+  description: 'Created by yassine ben naya',
   generator: 'v0.dev',
 }
 
